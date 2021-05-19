72f869c1fa50396206b57c398d7e5fff6d3ec30e
Agustin <ratto_agustin@hotmail.com>
2021-05-19 15:46:35 -0300

    conflicto resuelto ej6

--- ej6/ejercicio6.ps1
+++ ej6/ejercicio6.ps1
@@ -1,119 +1,102 @@
+<#
+    .SYNOPSIS
+        Papelera de reciclaje
+    
+    .DESCRIPTION
+        Permite eliminar archivos, recuperarlos, listarlos y vaciarla.
+        Se puede pasar solo un parametro a la vez.
+    
+    .PARAMETER [FILE]
+        Idica ruta de archivo a eliminar.
+    .PARAMETER l
+        Lista los archivos dentro de la papelera
+    .PARAMETER r
+        Indica el nombre del archivo a recuperar de la papelera.
+        Vuelve a su ubicacion original
+    .PARAMETER e
+        Permite detener el evento
+        No se puede pasar con los otros parametros
+
+    .EXAMPLE
+        ./mueveArchivo.ps1 -Descargas ../descargas/carpetaPrueba/ -DirectorioDestino ../descargas/carpetaPrueba/destino/
+    .EXAMPLE
+        ./mueveArchivo.ps1 -Descargas ../descargas/carpetaPrueba/
+    .EXAMPLE
+        ./mueveArchivo.ps1 -Detener 
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false,
+    ValueFromPipeline=$true,Position=0,ParameterSetName="param0")]
+    [ValidateScript({Test-Path -Path $_})]
+    [System.IO.FileInfo]
+    $archivoAEliminar=$null,
+
+    [Parameter(Mandatory=$false,ParameterSetName="param1")]
+    [Switch]
+    $l,
+    
+    [Parameter(Mandatory=$false,ParameterSetName="param2")]
+    [System.IO.FileInfo]
+    #[ValidateScript({Test-Path -Path $_})]
+    $r=$null,
+    
+    [Parameter(Mandatory=$false,ParameterSetName="param3")]
+    [Switch]
+    $e
+)
+
 Add-Type -Assembly System.IO.Compression.FileSystem
-function mostrarAyuda()
-{
-
-    param(
-        [string[]]$1
-    )
-
-    if ( ($1 -eq"-h") || ($1 -eq "-help")  || ($1 -eq "-?") ){
-
-            Write-Host "Uso: $0"
-            Write-Host ""
-            Write-Host "Argumentos:"
-            Write-Host ""
-            Write-Host "      -[FILE]"$'\t'"elimina el archivo llevandolo a la papelera de reciclaje."
-            Write-Host "      -l"$'\t'"lista los archivos contenidos en la papelera de reciclaje."
-            Write-Host "      -r [FILE]"$'\t'"recupera el archivo."
-            Write-Host "      -e"$'\t'"vacia la papelera de reciclaje."
-            exit 0
-
-    }
-}
-
-
-
-function verificarParametros($1, $2)
-{
-    
-
-    if ($1 -ne '-l' -and $1 -ne '-r' -and $1 -ne '-e' -and  -not(Test-Path -Path $1) ) {
-        # chequear porque no funciona simepre con rutas absolutas ???
-
-        Write-Host "\"$1\" no es una opcion valida o es un archivo inexistente"
-        exit 1
-    }
-
-    
-     if ((Test-Path -Path $1) -and "$2" -ne "" ){
-
-        Write-Host "Error, si busca eliminar un archivo, debe enviar como parametro solo [FILE]"
-        exit 1
-    }
-
-    if  ("$1" -eq '-l' -and "$2" -ne "" ){
-
-        Write-Host "Error, el parametro "-l" debe ser enviado solo"
-        exit 1
-    }
-
-    if  ("$1" -eq '-r' -and "$2" -eq "" ){
-
-        Write-Host "Error, el parametro "-r" debe estar acompañado de un nombre de archivo"
-            exit 1
-    }
-
-    if  ("$1" -eq '-e' -and "$2" -ne "") {
-
-        Write-Host "Error, el parametro "-e" debe ser enviado solo"
-        exit 1
-    }
-}
-
-if ( $args.Count -gt 2 ){
-
-    Write-Host "Error, como maximo se pueden pasar 2 parametros."
-    Write-Host "Indique -h para obtener mas informacion sobre los parametros"
-    exit 1
-}
-
-
-mostrarAyuda $args[0]
-verificarParametros $args[0] $args[1]
-
 
 $directorioPapelera = $HOME+'/Papelera';
-#$directorioPapelera1 = $HOME+'/salida.txt';
 $destino=$HOME+'\Papelera.zip';
 $listar=$HOME+'\Papelera';
 
 
 Write-Host "Directorio de la papelera: $directorioPapelera"
 
-if (Test-Path -Path $args[0]){
-    
+if ($archivoAEliminar -ne $null)
+{    
     #Mandar el archivo a la papelera
-    $pathAbsoluto = (Get-ChildItem $args[0] | Select-Object FullName).FullName
-    $pathAbsoluto -match "(?<=/).[^/]*$";
-    $pathAbsoluto -match ".*(?=/)";
+    $pathAbsoluto = (Get-ChildItem $archivoAEliminar | Select-Object FullName).FullName
+    $pathAbsoluto -match "(?<=/).[^/]*$" | Out-Null
+    $pathAbsoluto -match ".*(?=/)" | Out-Null
 
     zip -m $HOME/Papelera.zip $pathAbsoluto *> /dev/null
+
+    Write-Host $archivoAEliminar.Name "eliminado" -ForegroundColor Magenta
 }
 
-if ( $args[0] -eq '-l'){
+if ($l -eq $true){
     #listar los archivos de la papelera
     Expand-Archive -Path $destino -DestinationPath $directorioPapelera *> /dev/null
 
     $archivos = Get-ChildItem -Path $listar -Recurse  -File
 
-    Write-Host "~~~~~~" `t`t "~~~~" -ForegroundColor Green
-    Write-Host "Nombre" `t`t "Ruta"
-    Write-Host "~~~~~~" `t`t "~~~~" -ForegroundColor Green
-
-    foreach ($item in $archivos) {
-        Write-Host $item.Name `t ($item.Directory |Select-String -Pattern '(?<=Papelera\/).+' -All).Matches.value
+    if($archivos.Count -eq 0)
+    {
+        Write-Host "Papelera vacia" -ForegroundColor Green
+    }
+    else {
+        Write-Host "~~~~~~" `t`t "~~~~" -ForegroundColor Green
+        Write-Host "Nombre" `t`t "Ruta"
+        Write-Host "~~~~~~" `t`t "~~~~" -ForegroundColor Green
+    
+        foreach ($item in $archivos) {
+            Write-Host $item.Name `t ($item.Directory |Select-String -Pattern '(?<=Papelera\/).+' -All).Matches.value
+        }
     }
 
     Remove-Item -Path $directorioPapelera -Recurse
 }
-if($args[0] -eq "-e")
+if($e -eq $true)
 {
     zip -d $HOME/Papelera.zip "*" *> /dev/null
 }
 
-if($args[0] -eq "-r")
+if($r -ne $null)
 {
-    $archivoBuscado = $args[1]
+    $archivoBuscado = $r
     Expand-Archive -Path $destino -DestinationPath $directorioPapelera *> /dev/null
     $archivos = Get-ChildItem -Path $listar -Recurse  -File 
 
@@ -148,8 +131,10 @@ if($args[0] -eq "-r")
 
     $directorioReal = ($archivo.FullName |Select-String -Pattern '(?<=Papelera\/).+' -All).Matches.value
 
-    unzip -p $HOME/Papelera.zip $directorioReal > /$directorioReal #*> /dev/null
-    zip -d $HOME/Papelera.zip $directorioReal #*> /dev/null
+    unzip -p $HOME/Papelera.zip $directorioReal > /$directorioReal *> /dev/null
+    zip -d $HOME/Papelera.zip $directorioReal *> /dev/null
+
+    Write-Host $archivo.Name "restaurado a su ubicacion original" -ForegroundColor Green
 
     Remove-Item -Path $directorioPapelera -Recurse
 }
