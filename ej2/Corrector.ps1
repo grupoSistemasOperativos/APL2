f2bcf03cf9a03effda1d8ccf33caa2abc062d5ae
Agustin <ratto_agustin@hotmail.com>
2021-05-19 17:04:44 -0300

    fix rutas y parametros

--- ej2/Corrector.ps1
+++ ej2/Corrector.ps1
@@ -8,42 +8,50 @@
         ./Corrector.ps1 -In prueba/hola.txt
 #>
 
-$pathAbsoluto = (Get-ChildItem $args[1] | Select-Object FullName).FullName;
-$pathAbsoluto -match ".*(?=\\)" | Out-Null;
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateScript({Test-Path $_})]
+    [String]
+    $in
+)
+
+$pathAbsoluto = (Get-ChildItem $in | Select-Object FullName).FullName;
+$pathAbsoluto -match ".*(?=/)" | Out-Null;
 $ruta = $Matches[0];
-$pathAbsoluto -match "(?<=\\).[^\\]*$"| Out-Null;
+$pathAbsoluto -match "(?<=/).[^/]*$"| Out-Null;
 $nombreArchivo = $Matches[0];
 $nombreArchivo -match ".*(?=\.)"| Out-Null;
 $nombreArchivoSinExtension = $Matches[0];
 $nombreArchivo -match "\.([^.]*?)(?=\?|#|$)"| Out-Null;
 $extension = $Matches[0];
-$pathLog= $nombreArchivo + ".log"
 
 if($Matches[0] -eq $nombreArchivo){
     $extension=""
- }
+}
 
 $dia = Get-Date -Format 'yyyyMMddHHmm';
-$nombreArchivoResultado= $ruta + '\' + $nombreArchivoSinExtension + "_" + $dia + $extension;
+$nombreArchivoResultado= $ruta + '\' + $nombreArchivoSinExtension + "_" + $dia;
+$pathLog= $nombreArchivoResultado + ".log"
+$nombreArchivoResultado= $nombreArchivoResultado + $extension
 
 $text = get-content $pathAbsoluto 
 
 get-content $pathAbsoluto | ForEach-Object{$_ -replace "[ ]{2,}"," " ` -replace " \.", "." ` -replace " \,", "," ` -replace " \;", ";" ` -replace " \.", "." ` -replace "\.(?=[0-9a-z])", ". " ` -replace "\,(?=[0-9a-z])", ", " ` -replace "\;(?=[0-9a-z])", "; "} | Set-Content $nombreArchivoResultado
-                                                
+                                         
 $contarInconsistencias = [regex]::Matches($text, "[ ]{2,}").Count + [regex]::Matches($text, " [.;,]").Count + [regex]::Matches($text, "[.;,][^ ]").Count 
 
 
 $parentesis = [regex]::Matches($text, "\(").Count - [regex]::Matches($text, "\)").Count
 $parentesis = [Math]::Abs($parentesis)
-Write-Host $parentesis
 
 $preguntas = [regex]::Matches($text, "\?").Count - [regex]::Matches($text, "\¿").Count
 $preguntas = [Math]::Abs($preguntas)
-Write-Host $preguntas
+
 
 $admiracion = [regex]::Matches($text, "\!").Count - [regex]::Matches($text, "\¡").Count
 $admiracion = [Math]::Abs($admiracion)
-Write-Host $admiracion
+
 
 # $parentesis = contarInconsistencias("\(", "\)")
 # $preguntas = contarInconsistencias("\?", "\¿")
@@ -54,4 +62,6 @@ $texto += "La cantidad de parentesis dispares es: $parentesis.`n"
 $texto += "La cantidad de signos de pregunta dispares es: $preguntas.`n"
 $texto += "La cantidad de signos de exclamacion dispares es: $admiracion."
 
-New-Item -Path $ruta -Name $pathLog -ItemType "file" -Value $texto | Out-Null;
+New-Item -Path $pathLog -ItemType "file" -Value $texto -Force| Out-Null;
+
+Write-Host "Correccion efectuada con exito" -ForegroundColor Green
